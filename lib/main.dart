9fbea75daa70b5d54dbd8baca2fa1b4031f2662f
Melmohands22 <melmohands591@gmail.com>
2024-08-16 16:16:16 +0300

    add constans and them color to all project

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:booklyapp/Features/home/presentation/view_models/views/home_screen.dart';
 import 'package:booklyapp/Features/splash/presentation/view_models/views/splash_view.dart';
+import 'package:booklyapp/constans.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
@@ -14,8 +15,11 @@ class BooklyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+        theme: ThemeData(
+          scaffoldBackgroundColor: kPrimaryColor,
+        ),
         debugShowCheckedModeBanner: false,
-        home: SplashView(),
+        home: const SplashView(),
         routes: {
           HomeView.id: (context) => HomeView(),
         });
