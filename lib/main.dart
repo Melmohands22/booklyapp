311aa50cee09dc7858cb39c666625973ee3cee81
Melmohands22 <melmohands591@gmail.com>
2024-08-16 13:24:40 +0300

    start project , add get package , home view , splash view and splash view body

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:booklyapp/Features/home/presentation/view_models/views/home_screen.dart';
+import 'package:booklyapp/Features/splash/presentation/view_models/views/splash_view.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+void main() {
+  runApp(const BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: SplashView(),
+        routes: {
+          HomeView.id: (context) => HomeView(),
+        });
+  }
+}
