496bfb8ea469c571e345312bb01d430565dc54cc
Melmohands22 <melmohands591@gmail.com>
2024-08-20 10:43:17 +0300

    best seller list view p1

--- lib/Features/home/presentation/view_models/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/view_models/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:booklyapp/Core/utils/assets.dart';
 import 'package:booklyapp/Core/utils/styles.dart';
 import 'package:booklyapp/Features/home/presentation/view_models/views/widgets/custom_appbar.dart';
 import 'package:booklyapp/Features/home/presentation/view_models/views/widgets/featured_books_list_view.dart';
@@ -13,12 +14,42 @@ class HomeViewBody extends StatelessWidget {
       children: const [
         CustomAppbar(),
         FeaturedBooksListView(),
-        const SizedBox(height: 50),
+        SizedBox(height: 50),
         Text(
           'Best Seller',
           style: Styles.mediumTitle,
-        )
+        ),
+        SizedBox(height: 30),
+        BestSellerListView()
       ],
+    );
+  }
+}
+
+class BestSellerListView extends StatelessWidget {
+  const BestSellerListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 130,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  image: DecorationImage(
+                    fit: BoxFit.fill,
+                    image: AssetImage(
+                      AssetsDataa.testImage,
+                    ),
+                  )),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
